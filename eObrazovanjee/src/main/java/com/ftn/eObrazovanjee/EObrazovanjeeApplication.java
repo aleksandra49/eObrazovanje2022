5d23495883a0ce4af4689842c9f17af2a8385226
Aleksandra Radak <anja.9811@gmail.com>
2022-04-10 13:50:31 +0200

    inicijalni komit praznog projekta

---
+++ eObrazovanjee/src/main/java/com/ftn/eObrazovanjee/EObrazovanjeeApplication.java
@@ -0,0 +1,13 @@
+package com.ftn.eObrazovanjee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EObrazovanjeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EObrazovanjeeApplication.class, args);
+	}
+
+}
---
+++ eObrazovanjee/src/test/java/com/ftn/eObrazovanjee/EObrazovanjeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftn.eObrazovanjee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EObrazovanjeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
