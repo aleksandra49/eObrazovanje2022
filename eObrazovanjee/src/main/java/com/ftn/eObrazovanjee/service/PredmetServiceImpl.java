6c499529f21c80477fd71898abb7a48d436a97e0
Petar Medic <mpetar97@gmail.com>
2022-04-12 11:16:54 +0200

    # WARNING: head commit changed in the meantime
    Merge branch 'master' of
    https://github.com/aleksandra49/eObrazovanje2022.git

--- eObrazovanjee/src/main/java/com/ftn/eObrazovanjee/service/PredmetServiceImpl.java
+++ eObrazovanjee/src/main/java/com/ftn/eObrazovanjee/service/PredmetServiceImpl.java
@@ -1,6 +1,34 @@
 package com.ftn.eObrazovanjee.service;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ftn.eObrazovanjee.model.Predmet;
+import com.ftn.eObrazovanjee.repository.PredmetRepository;
+
+@Service
 public class PredmetServiceImpl {
 
+	@Autowired
+	PredmetRepository repository;
+	
+	public Predmet findOne(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+	
+	public List<Predmet> findAll() {
+
+		return repository.findAll();
+	}
+	
+	public void save(Predmet predmet) {
+		repository.save(predmet);
+		
+	}
+	
+	public void remove(Long id){
+		repository.deleteById(id);
+	}
 }
--- eObrazovanjee/src/main/java/com/ftn/eObrazovanjee/service/ProfesorServiceImpl.java
+++ eObrazovanjee/src/main/java/com/ftn/eObrazovanjee/service/ProfesorServiceImpl.java
@@ -1,6 +1,34 @@
 package com.ftn.eObrazovanjee.service;
 
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.ftn.eObrazovanjee.model.Profesor;
+import com.ftn.eObrazovanjee.repository.ProfesorRepository;
+
+@Service
 public class ProfesorServiceImpl  {
+	
+	@Autowired
+	ProfesorRepository repository;
+	
+	public Profesor findOne(Long id) {
+		return repository.findById(id).orElse(null);
+	}
+	
+	public List<Profesor> findAll() {
 
+		return repository.findAll();
+	}
+	
+	public void save(Profesor profesor) {
+		repository.save(profesor);
+		
+	}
+	
+	public void remove(Long id){
+		repository.deleteById(id);
+	}
 }
